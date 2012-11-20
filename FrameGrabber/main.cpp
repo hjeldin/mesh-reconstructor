c90934c742dfb7d385d886266ac94725b3b341bc
kinect1 <kinect@kinect-System-Product-Name.(none)>
2012-11-20 16:21:29 +0100

    added FrameGrabber project

---
+++ FrameGrabber/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: kinect
+ *
+ * Created on 20 novembre 2012, 15.49
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
